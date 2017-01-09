44282e49c8e82b1bc3bf130a91dc45074ee0b1dc
Ziv Zamechek <ziv921@yahoo.com>
2017-01-09 15:53:22 -0500

    Display output in React with a components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React from 'react';
 class App extends React.Component {
   // return DOM representation of the component
   render() {
-    return <h1>Hello World</h1>
-    // render React.createElement('h1', null, 'Hello World')
+    return (
+      <div>
+        <h1>Hello World!!!</h1>
+        <b>Bold</b>
+      </div>
+      
+      // this is the function that we are returning
+      // render React.createElement('h1', null, 'Hello World')
+    ) 
   }
 }
 // can have state
