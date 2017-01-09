41c88998e987e56f2963338c257f8c89cb479b26
Ziv Zamechek <ziv921@yahoo.com>
2017-01-09 15:48:30 -0500

    Wrote Hello World React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
-
+class App extends React.Component {
+  // return DOM representation of the component
+  render() {
+    return <h1>Hello World</h1>
+    // render React.createElement('h1', null, 'Hello World')
+  }
+}
+// can have state
+// const App = () => <h1>Hello World</h1>
 export default App
