2d1e44ddf46d1b7c64db9e51ee4352a53777c0f8
Ziv Zamechek <ziv921@yahoo.com>
2017-01-09 16:11:02 -0500

    Managed React Component State with setState

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 class App extends React.Component {
   // return DOM representation of the component
+  // passed values and managed by the components
+  constructor() {
+    // this will get the component
+    super();
+    this.state = {
+      stateTxt: 'this is the state txt',
+      cat: 0
+    }
+  }
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
   render() {
     let txt = this.props.txt
     return (
@@ -9,6 +21,9 @@ class App extends React.Component {
         <h1>Hello World!!!</h1>
         <b>Bold</b>
         <h1>{txt}</h1>
+        <input type="text" 
+        onChange={this.update.bind(this)}/>
+        <h2>{this.state.stateTxt} - {this.state.cat}</h2>
       </div>
       
       // this is the function that we are returning
